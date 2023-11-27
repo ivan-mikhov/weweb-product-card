12284c0a27204d399eda395ed0d6e239c3d5fce2
Ivan Mikhov <ivan.mikhov@connectish.de>
2023-11-28 00:19:00 +0100

    Downgrade to demo clean code

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,26 +1,6 @@
 <template>
   <div class="my-element">
     <p :style="textStyle">I am a custom element !</p>
-    <SfProductCard
-        image="/assets/storybook/Home/productB.jpg"
-        :imageWidth="216"
-        :imageHeight="326"
-        badgeLabel=""
-        badgeColor=""
-        title="Cotton Sweater"
-        linkTag=""
-        :scoreRating="4"
-        :reviewsCount="7"
-        :maxRating="5"
-        regularPrice=""
-        specialPrice=""
-        wishlistIcon="heart"
-        isInWishlistIcon="heart_fill"
-        :isInWishlist="false"
-        showAddToCartButton
-        :isAddedToCart="false"
-        :addToCartDisabled="false"
-    />
   </div>
 </template>
 
