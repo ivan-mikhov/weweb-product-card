bc88d1b6ccac1d257f1ac205a90b9e6d44662739
Ivan Mikhov <ivan.mikhov@connectish.de>
2023-11-28 03:43:22 +0100

    Tailwind static build

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,35 +1,99 @@
 <template>
-  <SfProductCard
-      image="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/smartwatch.png"
-      :imageWidth="216"
-      :imageHeight="326"
-      badgeLabel=""
-      badgeColor=""
-      title="Cotton Sweater"
-      linkTag=""
-      :scoreRating="4"
-      :reviewsCount="7"
-      :maxRating="5"
-      regularPrice=""
-      specialPrice=""
-      wishlistIcon="heart"
-      isInWishlistIcon="heart_fill"
-      :isInWishlist="false"
-      showAddToCartButton
-      :isAddedToCart="false"
-      :addToCartDisabled="false"
-  />
+<!--  <wwLayoutItemContext-->
+<!--      is-repeat-->
+<!--      :data="item"-->
+<!--      :item="null"-->
+<!--      :index="index"-->
+<!--      v-for="(item, index) in content.data"-->
+<!--      :key="index">-->
+<!--    <wwElement v-bind="content.itemContainer"></wwElement>-->
+<!--  </wwLayoutItemContext>-->
+  <div
+      class="border border-neutral-200 rounded-md hover:shadow-lg max-w-[300px]"
+  >
+    <div class="relative">
+      <SfLink href="#" class="block">
+        <img
+            src="https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/sneakers.png"
+            alt="Great product"
+            class="block object-cover h-auto rounded-md aspect-square"
+            width="300"
+            height="300"
+        />
+      </SfLink>
+      <SfButton
+          variant="tertiary"
+          size="sm"
+          square
+          class="
+          absolute
+          bottom-0
+          right-0
+          mr-2
+          mb-2
+          bg-white
+          ring-1 ring-inset ring-neutral-200
+          !rounded-full
+        "
+          aria-label="Add to wishlist"
+      >
+        <SfIconFavorite size="sm"/>
+      </SfButton>
+    </div>
+    <div class="p-4 border-t border-neutral-200">
+      <SfLink href="#" variant="secondary" class="no-underline">
+        Athletic mens walking sneakers
+      </SfLink>
+      <div class="flex items-center pt-1">
+        <SfRating size="xs" :value="5" :max="5"/>
+
+        <SfLink href="#" variant="secondary" class="pl-1 no-underline">
+          <SfCounter size="xs">123</SfCounter>
+        </SfLink>
+      </div>
+      <p
+          class="
+          block
+          py-2
+          font-normal
+          leading-5
+          typography-text-sm
+          text-neutral-700
+        "
+      >
+        Lightweight • Non slip • Flexible outsole • Easy to wear on and off
+      </p>
+      <span class="block pb-2 font-bold typography-text-lg">$2345,99</span>
+      <SfButton size="sm" :style="buttonStyle">
+        <template #prefix>
+          <SfIconShoppingCart size="sm"/>
+        </template>
+        Add to cart
+      </SfButton>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import {
+  SfRating,
+  SfCounter,
+  SfLink,
+  SfButton,
+  SfIconShoppingCart,
+  SfIconFavorite,
+} from '@storefront-ui/vue';
+</script>
 
 <script>
 export default {
   props: {
-    content: { type: Object, required: true },
+    content: {type: Object, required: true},
   },
   computed: {
-    textStyle() {
+    buttonStyle() {
       return {
-        color: this.content.textColor,
+        backgroundColor: this.content.buttonColor,
       };
     },
   },
@@ -37,6 +101,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "tailwind.css";
+
 .my-element {
   p {
     font-size: 18px;
--- ww-config.js
+++ ww-config.js
@@ -1,16 +1,49 @@
 export default {
+  inherit: "ww-layout",
   editor: {
     label: {
-      en: "My Element",
+      en: "VSF: Product card",
     },
   },
   properties: {
-    textColor: {
+    buttonColor: {
       label: {
-        en: "Text color",
+        en: "Button color",
       },
       type: "Color",
-      defaultValue: "#F23636",
+      defaultValue: "#018937"
     },
+    data: {
+      label: {
+        en: 'Items',
+      },
+      type: 'Info',
+      options: {
+        text: { en: 'Bind your data' },
+      },
+      bindable: 'repeatable',
+      defaultValue: [],
+      section: 'settings',
+      /* wwEditor:start */
+      bindingValidation: {
+        validations: [
+          {
+            type: 'array',
+          },
+          {
+            type: 'object',
+          },
+        ],
+        tooltip: 'A collection or an array of data: \n\n`myCollection` or `[{}, {}, ...]`',
+      },
+      /* wwEditor:end */
+    },
+    // itemContainer: {
+    //   hidden: true,
+    //   defaultValue: {
+    //     isWwObject: true,
+    //     type: "ww-flexbox"
+    //   }
+    // }
   },
 };
